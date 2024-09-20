6c44bb9846d3f5111729f6254c0294a38bd4239d
Steven Chu <stevenc1@gmail.com>
2024-09-20 15:59:32 -0700

    Back in

--- scan.py
+++ scan.py
@@ -49,9 +49,9 @@ class KalmanFilter:
         kalman_gain = self.error / (self.error + self.measurement_variance)
         self.estimate = self.estimate + kalman_gain * (measurement - self.estimate)
         self.error = (1 - kalman_gain) * self.error + self.process_variance
-        # logger.debug(
-        #     f"Kalman Filter updated: RSSI={measurement}, Estimated RSSI={self.estimate}"
-        # )
+        logger.debug(
+            f"Kalman Filter updated: RSSI={measurement}, Estimated RSSI={self.estimate}"
+        )
         return self.estimate
 
 
