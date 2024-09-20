0c994728afa5b5120e8eeb5afd1fe5f7bf97d20f
Steven Chu <stevenc1@gmail.com>
2024-09-20 15:39:29 -0700

    Threshold

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
+threshold_detection_distance_m = 0.1524
+
 kalman_config = {
-    'process_variance': 1e-3,
-    'measurement_variance': 4.0,
-    'initial_estiamate':0.0,
-    'initial_error':1.0
+    "process_variance": 1e-3,
+    "measurement_variance": 4.0,
+    "initial_estiamate": 0.0,
+    "initial_error": 1.0,
 }
 
 target_devices = {
-    '64:e8:33:80:c3:5a': True,
-    '10:06:1c:b5:64:42': True,
-    'e8:6b:ea:cb:0e:12': True
+    "64:e8:33:80:c3:5a": True,
+    "10:06:1c:b5:64:42": True,
+    "e8:6b:ea:cb:0e:12": True,
 }
